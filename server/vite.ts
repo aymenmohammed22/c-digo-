2d84920fc68696c75e9456ed82ffe8b78de56f9c
aymenmohammed22 <aimenmo123@gmail.com>
2025-09-16 22:23:09 +0300

    Add files via upload

--- server/vite.ts
+++ server/vite.ts
@@ -5,7 +5,7 @@ import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
 import { type Server } from "http";
-import viteConfig from "../vite.config.js";
+import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
 import { fileURLToPath } from "url";
 
